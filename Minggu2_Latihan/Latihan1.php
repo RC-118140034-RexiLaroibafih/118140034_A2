172ea9e80ffac220dc908d0a054d5c48456264a5
RC-118140034-RexiLaroibafih <rexilaroibafih@gmail.com>
2020-11-14 22:52:35 +0700

    Minggu 2

---
+++ Minggu2_Latihan/Latihan1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <title>Latihan Nomor 1</title>
+    </head>
+<body>
+    <?php 
+	$bil1 = 20;
+    $bil2 = 5;
+    
+    echo "bilangan 1 = $bil1 <br>";
+	echo "bilangan 2 = $bil2 <br><br>";
+	echo "Berikut merupakan hasil dari setiap operasi <br><br>";
+	echo "PENJUMLAHAN <br>";
+	echo "Operator : + <br>";
+	echo "Hasil : ".$Hasil= $bil1+$bil2."<br><br>";
+
+	echo "PENGURANGAN <br>";
+	echo "Operator : - <br>";
+	echo "Hasil : ".$Hasil=$bil1-$bil2."<br><br>";
+
+	echo "PERKALIAN <br>";
+	echo "Operator : * <br>";
+	echo "Hasil : ".$Hasil=$bil1*$bil2."<br><br>";
+
+	echo "PEMBAGIAN <br>";
+	echo "Operator : / <br>";
+	echo "Hasil : ".$Hasil=$bil1/$bil2."<br><br>";
+
+	echo "MODULUS <br>";
+	echo "Operator : % <br>";
+	echo "Hasil : ".$Hasil = $bil1 % $bil2."<br><br>";
+     ?>
+ </body>
+ </html>
